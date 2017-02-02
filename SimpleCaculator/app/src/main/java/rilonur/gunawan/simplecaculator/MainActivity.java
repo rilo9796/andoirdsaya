19c135de85681828fa038b466452743b2acd4a14
rilo9796 <rilo9796@gmail.com>
2017-02-02 08:15:33 +0700

    Java Caculator Selesai

--- SimpleCaculator/app/src/main/java/rilonur/gunawan/simplecaculator/MainActivity.java
+++ SimpleCaculator/app/src/main/java/rilonur/gunawan/simplecaculator/MainActivity.java
@@ -29,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
             hasil = angka1 + angka2;
         } else if (opr == R.id.minus){
             hasil = angka1 - angka2;
+        }else if (opr == R.id.kali){
+            hasil = angka1 * angka2;
+        }else if (opr == R.id.bagi){
+            hasil = angka1 / angka2;
         }
         result.setText(Double.toString(hasil));
     }
