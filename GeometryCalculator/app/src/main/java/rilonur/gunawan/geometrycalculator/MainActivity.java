cb61c52b46f561d47ff480e553f484810d9abe95
rilo9796 <rilo9796@gmail.com>
2017-02-03 08:15:42 +0700

    Geometry Selesai

--- GeometryCalculator/app/src/main/java/rilonur/gunawan/geometrycalculator/MainActivity.java
+++ GeometryCalculator/app/src/main/java/rilonur/gunawan/geometrycalculator/MainActivity.java
@@ -27,14 +27,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void calculate(View view){
         EditText input1 = (EditText) findViewById(R.id.input1);
         EditText input2 = (EditText) findViewById(R.id.input2);
+        EditText input3 = (EditText) findViewById(R.id.input3);
         Spinner spinner = (Spinner) findViewById(R.id.geometry);
-        double inp2 = 0, inp1 =0;
+        double inp2 = 0, inp1 =0, inp3 =0;
 
         String operator = spinner.getSelectedItem().toString();
         String hasil = "";
         inp1 = Double.parseDouble(input1.getText().toString());
-        if(input2.isEnabled()){
-            inp2 =Double.parseDouble(input2.getText().toString());
+        if(input2.isEnabled()) {
+            inp2 = Double.parseDouble(input2.getText().toString());
+        }
+        if (input3.isEnabled()){
+            inp3 =Double.parseDouble(input3.getText().toString());
         }
 
         if(operator.equalsIgnoreCase("Lingkaran")){
@@ -44,12 +48,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             hasil= "Luas dari Segitiga Siku-siku adalah: "+(0.5*(inp1*inp2))+"\n";
             double hyp = Math.sqrt((inp1*inp1)+(inp2*inp2));
             hasil += "Keliling dari Segitiga Siku-siku adalah : "+(inp1+inp2+hyp);
-        }else{
+        }else if (operator.equalsIgnoreCase("Persegi")){
             hasil = "Luas dari Persegi adalah: "+(inp1*inp2)+"\n";
             hasil += "Kelilingan dari Pesergi adalah: "+((2*inp1)+(2*inp2));
+        }else  if (operator.equalsIgnoreCase("Balok")){
+            hasil = "Luas dari Balok adalah: "+(2*((inp1*inp2)+(inp1*inp3)+(inp2*inp3)))+"\n";
+            hasil += "Volume dari Balok adalah: "+(inp1*inp2*inp3)+"\n";
+        }else {
+            hasil = "Luas dari Bola adalah: "+(4*(Math.PI*(inp1*inp1)))+"\n";
+            hasil += "Volume dari Bola adalah: "+4/3*(Math.PI*(inp1*inp1*inp1))+"\n";
         }
 
-        TextView result = (TextView) findViewById(R.id.result);
+            TextView result = (TextView) findViewById(R.id.result);
         result.setText(hasil);
     }
 
@@ -57,20 +67,41 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         TextView text1 = (TextView) findViewById(R.id.txt1);
         TextView text2 = (TextView) findViewById(R.id.txt2);
+        TextView text3 = (TextView) findViewById(R.id.txt3);
         EditText input2 = (EditText) findViewById(R.id.input2);
+        EditText input3 = (EditText) findViewById(R.id.input3);
         String operator = parent.getItemAtPosition(position).toString();
         if(operator.equalsIgnoreCase("Lingkaran")){
             text1.setText("Jari-Jari");
             input2.setEnabled(false);
+            input3.setEnabled(false);
             text2.setText("");
+            text3.setText("");
         }else if(operator.equalsIgnoreCase("Segitiga")){
             text1.setText("Alas");
             text2.setText("Tinggi");
+            text3.setText("");
             input2.setEnabled(true);
-        }else{
+            input3.setEnabled(false);
+        }else if(operator.equalsIgnoreCase("Persegi")){
             text1.setText("Panjang");
             text2.setText("Lebar");
+            text3.setText("");
             input2.setEnabled(true);
+            input3.setEnabled(false);
+
+        }else if(operator.equalsIgnoreCase("Balok")) {
+            text1.setText("Panjang");
+            text2.setText("Lebar");
+            text3.setText("Tinggi");
+            input2.setEnabled(true);
+            input3.setEnabled(true);
+        }else {
+            text1.setText("Jari-Jari");
+            input2.setEnabled(false);
+            input3.setEnabled(false);
+            text2.setText("");
+            text3.setText("");
         }
     }
 
---
+++ MultiFuntionApp/app/src/main/java/rilonur/gunawa/multifuntionapp/ShowMassege.java
@@ -0,0 +1,13 @@
+package rilonur.gunawa.multifuntionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowMassege extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_massege);
+    }
+}
